a35099777756a261d35ef05cf79f4a623855db28
v-giorgio7 <vgiorgio@ciandt.com>
2022-05-16 13:45:40 -0300

    update: printFormatador corrected

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -5,10 +5,6 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        Locale ptBr = new Locale("pt", "BR");
-        String valorString = NumberFormat.getCurrencyInstance(ptBr).format(valor);
-        String valorFormatado= valorString.substring(0,8) + '.' + valorString.substring(9);
-        String valorFormatadoFinal = valorFormatado.substring(0,4) + ',' + valorFormatado.substring(5);
-        System.out.printf("Valor: %s\nTaxa: %.2f%%\n", valorFormatadoFinal, taxa);
+        System.out.printf("Valor: R$ %,.2f\nTaxa: %.2f%%\n", valor, taxa);
     }
 }
