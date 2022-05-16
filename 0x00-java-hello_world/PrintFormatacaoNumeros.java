f73be3adce24b6289afa114c610988c4781bbce4
v-giorgio7 <vgiorgio@ciandt.com>
2022-05-16 12:16:43 -0300

    update: formatacaoChanged to str

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -7,6 +7,8 @@ public class PrintFormatacaoNumeros {
         float valor = 7654.321f;
         Locale ptBr = new Locale("pt", "BR");
         String valorString = NumberFormat.getCurrencyInstance(ptBr).format(valor);
-        System.out.printf("Valor: %s\nTaxa: %.2f%%", valorString, taxa);
+        String valorFormatado= valorString.substring(0,8) + '.' + valorString.substring(9);
+        String valorFormatadoFinal = valorFormatado.substring(0,4) + ',' + valorFormatado.substring(5);
+        System.out.printf("Valor: %s\nTaxa: %.2f%%", valorFormatadoFinal, taxa);
     }
 }
