5e992a3379c2eed547940e4bd3c44eb41b015a54
v-giorgio7 <vgiorgio@ciandt.com>
2022-05-16 11:45:48 -0300

    feat: printConcat file added

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String texto1 = "Holberton";
+        String texto2 = "School";
+        String title = texto1.concat(" " + texto2);
+        System.out.println("Bem vindo a " + title + "!");
+    }
+}
