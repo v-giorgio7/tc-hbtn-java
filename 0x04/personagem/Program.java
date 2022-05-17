3eb5840199fde1ec9909f53a83375e3ec9f7b6fd
v-giorgio7 <vgiorgio@ciandt.com>
2022-05-17 18:12:47 -0300

    feat: task 2 added

--- 0x04/personagem/Program.java
+++ 0x04/personagem/Program.java
@@ -1,15 +1,10 @@
 public class Program {
     public static void main(String[] args) {
         PersonagemGame personagem1 = new PersonagemGame();
-        PersonagemGame personagem2 = new PersonagemGame();
 
-        personagem1.nome = "Sonic";
-        personagem1.saudeAtual = 10;
+        personagem1.setNome("Sonic");
+        personagem1.setSaudeAtual(10);
 
-        personagem2.nome = "Mario";
-        personagem2.saudeAtual = 100;
-
-        System.out.printf("Nome: %s - Saude: %d\n", personagem1.nome, personagem1.saudeAtual);
-        System.out.printf("Nome: %s - Saude: %d\n", personagem2.nome, personagem2.saudeAtual);
+        System.out.printf("Nome: %s - Saude: %d\n", personagem1.getNome(), personagem1.getSaudeAtual());
     }
 }
