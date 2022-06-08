9f13c1faa3ad5c63603e32493c4a4a373b29f6bf
v-giorgio7 <vgiorgio@ciandt.com>
2022-06-08 11:13:39 -0300

    bugfix: lambda tasks changed

--- Lambda_exercicios/consulta_produtos/ConsultaProdutos.java
+++ Lambda_exercicios/consulta_produtos/ConsultaProdutos.java
@@ -2,18 +2,19 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ConsultaProdutos {
     public static List<Produto> filtrar(List<Produto> produtos,
                                              CriterioFiltro criterioFiltro) {
-        List<Produto> listaProdutos = new ArrayList<Produto>();
+//        List<Produto> listaProdutos = new ArrayList<Produto>();
+//
+//        produtos.forEach(p -> {
+//            if (criterioFiltro.testar(p)) {
+//                listaProdutos.add(p);
+//            }
+//        });
 
-        produtos.forEach(p -> {
-            if (criterioFiltro.testar(p)) {
-                listaProdutos.add(p);
-            }
-        });
-
-        return listaProdutos;
+        return produtos.stream().filter(x -> criterioFiltro.testar(x)).collect(Collectors.toList());
     }
 }
--- Lambda_exercicios/predicado/ConsultaProdutos.java
+++ Lambda_exercicios/predicado/ConsultaProdutos.java
@@ -3,18 +3,25 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ConsultaProdutos {
     public static List<Produto> filtrar(List<Produto> produtos,
                                         Predicate<Produto> predicate) {
-        List<Produto> listaProdutos = new ArrayList<Produto>();
+//        List<Produto> listaProdutos = new ArrayList<Produto>();
+//
+//        produtos.forEach(p -> {
+//            if (predicate.test(p)) {
+//                listaProdutos.add(p);
+//            }
+//        });
 
-        produtos.forEach(p -> {
-            if (predicate.test(p)) {
-                listaProdutos.add(p);
-            }
-        });
+//        for (Produto produto : produtos) {
+//            if (predicate.test(produto)) {
+//                listaProdutos.add(produto);
+//            }
+//        }
 
-        return listaProdutos;
+        return produtos.stream().filter(x -> predicate.test(x)).collect(Collectors.toList());
     }
 }
