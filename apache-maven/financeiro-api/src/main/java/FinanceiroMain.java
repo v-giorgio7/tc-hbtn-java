b179fb124dbfe64b4c0e03f8f35884055bb0fe17
v-giorgio7 <vgiorgio@ciandt.com>
2022-06-15 13:16:52 -0300

    update: maven first project task added

--- apache-maven/financeiro-api/src/main/java/FinanceiroMain.java
+++ apache-maven/financeiro-api/src/main/java/FinanceiroMain.java
@@ -1,5 +1,5 @@
 public class FinanceiroMain {
     public static void main(String[] args) {
-        System.out.println("Criando um projeto Java Maven.");
+        System.out.println("Criando um projeto Java Maven");
     }
 }
